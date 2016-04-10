2a4f1f4700f302625a17f8dcf686a5dcfb4b880d
thomas-oo <thein.oo@mail.mcgill.ca>
2016-04-10 15:22:10 -0400

    Fixed some mistakes in player class & deleted unneeded classes
    Added a thread.sleep at the end while nav.isnavigating. Corrected
    waypoints when attackers/defenders are going to home

--- Robot testing/src/Robot/Defense.java
+++ Robot testing/src/Robot/Defense.java
@@ -1,60 +1,80 @@
 package Robot;
 
+import java.io.IOException;
+
+import lejos.hardware.BrickFinder;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.remote.ev3.RemoteRequestEV3;
+import lejos.robotics.RegulatedMotor;
 public class Defense 
 {
-	public static final EV3MediumRegulatedMotor defensemotor = new EV3MediumRegulatedMotor(LocalEV3.get().getPort("D"));
+	public EV3MediumRegulatedMotor defenseMotor;
 	final static int holdSpeed = 0;
 	final static int openSpeed = 100;
 	final static int holdacc = 2000;
 	final static int openacc = 100;
+	
+	public Defense()
+	{
+		String name = "slave";
+		RemoteRequestEV3 slave = null;
+		try {
+			slave = new RemoteRequestEV3(BrickFinder.find(name)[0].getIPAddress());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		defenseMotor = new EV3MediumRegulatedMotor(slave.getPort("C"));
+		//defenseMotor = slave.createRegulatedMotor("C", 'M');
+	}
+	
 	public void mode(int status) 
 	{
-		if (status==0)
+		if (status==0) //float
 		{
-			defensemotor.setAcceleration(0);
-			defensemotor.setSpeed(0);
-			defensemotor.flt();
+			defenseMotor.setAcceleration(0);
+			defenseMotor.setSpeed(0);
+			defenseMotor.flt();
 			System.out.println("Mode0");
 			return;
 		}
-		if (status==1)
+		if (status==1) //hold
 		{
-			defensemotor.setAcceleration(holdacc);
-			defensemotor.setSpeed(holdSpeed);	
-			defensemotor.lock(100);
+			defenseMotor.setAcceleration(holdacc);
+			defenseMotor.setSpeed(holdSpeed);	
+			defenseMotor.lock(100);
 			System.out.println("Mode1");
 			return;
 		}
-		if (status==2)
+		if (status==2) //open
 		{
 			System.out.println("Mode2");
-			defensemotor.setAcceleration(openSpeed);
-			defensemotor.setSpeed(openacc);	
-			defensemotor.rotateTo(90);
-			defensemotor.lock(50);
+			defenseMotor.setAcceleration(openSpeed);
+			defenseMotor.setSpeed(openacc);	
+			defenseMotor.rotateTo(90);
+			defenseMotor.lock(50);
 			//defensemotor.setAcceleration(holdacc);
 			//defensemotor.setSpeed(holdSpeed);	
 			
 			return;
 		}
-		if (status==3)
+		if (status==3) //close
 		{
 			System.out.println("Mode3");
-			defensemotor.setAcceleration(openSpeed);
-			defensemotor.setSpeed(openacc);	
-			defensemotor.rotateTo(0);
-			defensemotor.lock(50);
+			defenseMotor.setAcceleration(openSpeed);
+			defenseMotor.setSpeed(openacc);	
+			defenseMotor.rotateTo(0);
+			defenseMotor.lock(50);
 			//defensemotor.setAcceleration(holdacc);
 			//defensemotor.setSpeed(holdSpeed);	
 			
 			return;
 		}
-		if (status==4)
+		if (status==4) //???
 		{
 			System.out.println("Mode4");
-			defensemotor.lock(50);
+			defenseMotor.lock(50);
 			return;
 		}
 		return;
